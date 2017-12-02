203753fa142450c061794967c3c4747d92185609
unknown <240433360@qq.com>
2017-12-02 17:01:09 +0800

    update interface and commit

--- python/spiderMannager.py
+++ python/spiderMannager.py
@@ -2,52 +2,81 @@
 __author__ = 'winter'
 
 from time import ctime,sleep
-import os, sys
-from urlTools import UrlTools
-from fileTools import FileTools
-from logTools import  Log
+import os
+
+from utils.urlTools import UrlTools
+from utils.fileTools import FileTools
+from utils.logTools import  Log
 from code import systemCode
+
+
+
+
 # from parse.shenxuParse import ShenXuParse  python 3.5
-from parse.shenxuParse import ShenXuParse
-from novel.downloadnovelItem import DownLoadNovelItem
+from parse.biXiaWenXueParse import BiXiaWenXueParse
+from bean.novel.downloadnovelItem import DownLoadNovelItem
 
 class SpiderMannager(object):
     def __init__(self):
         pass
 
+    def __getNovelListFromConfig__(self):
+        fileTools = FileTools(systemCode.downloadNovelsInfoFile)
+        allNovels = fileTools.readFile()
+        Log.info("download file info "+allNovels)
+        novels = []
+        if allNovels !="":
+            list = allNovels.split('\r\n')
+            for index, raw in enumerate(list):
+                raw = raw.replace(' ', '')
+                if '#' in raw and raw[-1] == '/':
+                    tmpList = raw.split('#')
+                    baseUrl = tmpList[0]
+                    novelUrl = tmpList[1]
+                    item = DownLoadNovelItem(baseUrl, novelUrl)
+                    novels.append(item)
+                    Log.info("DownInfo is "+raw)
+                else:
+                    Log.error("DownInfo is not except!")
+        else:
+            Log.waring("DownInfo is null!")
+        return novels
+
     def manager(self):
         novels = []
-        shenXuItem = DownLoadNovelItem(systemCode.baseUrl,systemCode.shenXuNovelUrl, systemCode.shenXuFileName, systemCode.shenXuLocalFolder);
-        nineDayDrogen = DownLoadNovelItem(systemCode.baseUrl, systemCode.nineDayDrogenUrl, systemCode.nineDayDrogenFileName, systemCode.nineDayDrogenFolder)
-        novels.append(shenXuItem)
-        novels.append(nineDayDrogen)
         i = 1
         while True:
-            for index, novel in enumerate(novels):
-                print(u"download source page ")
-                Log.info(u"download source page ")
-                self.analysisNovelInfo(self.getNovel(novel), novel)
-                print(u"parse source page ")
-                Log.info(u"parse source page ")
-                novelUrl = novel.baseUrl
-                fileName = novel.fileName
-                localFolder = novel.localFolder
-                fileTools = FileTools(localFolder + "/" + fileName)
-                content = fileTools.readFile()
-                shenXuParse = ShenXuParse(content)
-                shenXuParse.setUrl(novelUrl)
-                shenXuParse.setLocalFolder(localFolder)
-                shenXuParse.parse()
-            print("no %s all over %s" %(i,ctime()))
-            Log.info("no %s all over %s" %(i,ctime()))
-            i = i + 1
+            novels = self.__getNovelListFromConfig__()
+            if len(novels) > 0:
+                for  index, novel in enumerate(novels):
+                    if systemCode.baseUrl in novel.baseUrl():  ## 陛下文学网
+                        print(u"download source page ")
+                        Log.info(u"download source page ")
+                        self.analysisNovelInfo(self.getNovel(novel), novel)
+                        print(u"parse source page ")
+                        Log.info(u"parse source page ")
+                        novelUrl = novel.baseUrl()
+                        fileName = novel.fileName()
+                        localFolder = novel.localFolder()
+                        fileTools = FileTools(localFolder + "/" + fileName)
+                        content = fileTools.readFile()
+                        bixiaParse = BiXiaWenXueParse(content)
+                        bixiaParse.setUrl(novelUrl)
+                        bixiaParse.setLocalFolder(localFolder)
+                        bixiaParse.parse()
+                    else:
+                        Log.error("now unsupport this network "+novel)
+                print("no %s all over %s" %(i,ctime()))
+                Log.info("no %s all over %s" %(i,ctime()))
+                i = i + 1
             sleep(3600*12)
 
 
     def getNovel(self,novel):
-        url = novel.novelUrl
-        fileName = novel.fileName
-        localFolder = novel.localFolder
+        url = novel.novelUrl()
+        fileName = novel.fileName()
+        localFolder = novel.localFolder()
+        print(localFolder)
         if not os.path.exists(localFolder):
             os.mkdir(localFolder)
         urlTools = UrlTools(url);
@@ -56,16 +85,18 @@ class SpiderMannager(object):
         fileTools.writeNewFile(content)
         return content
 
-    ##   name#url#author#imageurl#lashUpdateTime#lastUpdateChapter
+    ##   no#name#url#author#imageurl#lashUpdateTime#lastUpdateChapter
     def analysisNovelInfo(self, content, novel):
         Log.info("analysisNovelInfo start")
-        split= systemCode.fileContentSplit
+        split= str(systemCode.fileContentSplit)
+        novelNo=novel.novelNo()
         name = ''
-        url = novel.novelUrl
+        url = novel.novelUrl()
         author = ''
         imageurl = ''
         lashUpdateTime = ''
         lastUpdateChapter = ''
+        novelinfo = ''
         contentList = content.decode('utf-8').split('\r\n')
         for index, raw in enumerate(contentList):
             if '<h1>' in raw :
@@ -78,8 +109,8 @@ class SpiderMannager(object):
                 lashUpdateTime = raw.split('更新：')[1].replace('</p>', '')
             if ' <p>最新更新：' in raw :
                 lastUpdateChapter = raw.split('">')[1].replace('</a></p>', '')
-        novelinfo = name+ split +url+ split +author+ split \
-                    +imageurl+ split +lashUpdateTime+ split +lastUpdateChapter
+        novelinfo = str(novelNo)+split+str(name)+ split +str(url)+ split +str(author)+ split \
+                    +str(imageurl)+ split +str(lashUpdateTime)+ split +str(lastUpdateChapter)
         Log.info(novelinfo)
 
         fileTools = FileTools(systemCode.baseFolder+ '/SourceUrlFile/'+systemCode.allNovelsNameInfoFile)
