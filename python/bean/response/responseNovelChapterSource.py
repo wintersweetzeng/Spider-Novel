8ccb144cbe6fa3587da696417aa9109973f1cd0c
unknown <240433360@qq.com>
2017-12-03 01:27:37 +0800

    add new interface getChapterSource  and  spider support

---
+++ python/bean/response/responseNovelChapterSource.py
@@ -0,0 +1,8 @@
+#-*- conding:UTF-8 -*-
+__author__ = 'winter'
+
+## chapterSourceUrl#titleName
+class ResponseNovelChapterSource(object):
+    def __init__(self, chapterSourceUrl, titleName):
+        self.chapterSourceUrl = chapterSourceUrl
+        self.titleName = titleName
