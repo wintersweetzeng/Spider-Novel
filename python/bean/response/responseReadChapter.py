a19d24694fbe5091d78e12df322a95d6bb99de45
unknown <240433360@qq.com>
2017-12-06 19:54:02 +0800

    add login registry get user read info

---
+++ python/bean/response/responseReadChapter.py
@@ -0,0 +1,9 @@
+#-*- conding:UTF-8 -*-
+__author__ = 'winter'
+
+class ResponseReadChapter(object):
+    def __init__(self, id, novelNo, chapterNo, chapterTitle):
+        self.id = id
+        self.novelNo = novelNo
+        self.chapterNo = chapterNo
+        self.chapterTitle = chapterTitle
